49e999f2747628f567c4fa9fd5217c54ff2c691c
Hope Moore <hopemoore@gmail.com>
2021-01-19 11:55:49 -0500

    Add beginning of stair work
    Got to a point of being able to go up some stairs, but the stairs look HUGE.

---
+++ Assets/Scripts/FollowMovementCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMovementCollider : MonoBehaviour
+{
+    public Transform thingToFollow;
+    public float offset = 0.1431707F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, thingToFollow.position.y - offset, transform.position.z);
+    }
+}
